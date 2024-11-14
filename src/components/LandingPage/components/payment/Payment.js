e3d2cc093d6bffc26993a0a9f0ea34f488752774
Aashu-crypto <ashugandotra14@gmail.com>
2024-11-14 11:19:30 +0530

    updated webview

--- src/components/LandingPage/components/payment/Payment.js
+++ src/components/LandingPage/components/payment/Payment.js
@@ -52,34 +52,11 @@ function Payment() {
 
       let checkoutOptions = {
         paymentSessionId: json.paymentSessionID,
-        redirectTarget: "_modal", // optional (_self, _blank, or _top)
+        redirectTarget: "_self",
         returnUrl: `https://saathi.etheriumtech.com/payment/status?order_id=${json.orderID}`,
       };
 
-      cashfree.checkout(checkoutOptions).then((result) => {
-        console.log("result ", result);
-
-        if (result.error) {
-          // This will be true whenever user clicks on close icon inside the modal or any error happens during the payment
-          alert("User divya it is working");
-          console.log(
-            "User has closed the popup or there is some payment error, Check for Payment Status"
-          );
-          console.log(result.error);
-        }
-        if (result.redirect) {
-          // This will be true when the payment redirection page couldnt be opened in the same window
-          // This is an exceptional case only when the page is opened inside an inAppBrowser
-          // In this case the customer will be redirected to return url once payment is completed
-          console.log("Payment will be redirected");
-        }
-        if (result.paymentDetails) {
-          // This will be called whenever the payment is completed irrespective of transaction status
-          console.log("Payment has been completed, Check for Payment Status");
-          console.log(result.paymentDetails.paymentMessage);
-          navigate("/payment/success"); // Change to your desired route
-        }
-      });
+      cashfree.checkout(checkoutOptions)
 
       // Set order details after successful payment initialization
       setOrderDetail(json);
